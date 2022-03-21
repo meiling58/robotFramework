b4dadb7c1d0a7f1a0f86aeae80c6c417cc97a9ac
Mei-Ling Liang <67974179+meiling58@users.noreply.github.com>
2022-03-21 12:37:59 -0400

    Update common.py

--- PO/common.py
+++ PO/common.py
@@ -9,3 +9,24 @@ Firefox = "ff"
 
 # Define URL
 paychexHome = "https://www.paychex.com"
+
+# common field - Header
+paychex_home_image = "//img[@alt='Home']"
+Our_Solutions = "//div[normalize-space()='Our Solutions']"
+Resources = "//div[@class='navbar-link'][normalize-space()='Resources']"
+Our_Company = "//div[normalize-space()='Our Company']"
+Professional_Partnerships = "//div[normalize-space()='Professional Partnerships']"
+Contact_Us = "//div[normalize-space()='Contact Us']"
+Search_icon_image = "//img[@alt='Search icon']"
+Login = "//a[@class='button is-black']"
+Request_Pricing = "//a[@class='button is-paychexblue is-hidden-mobile']"
+
+# common field - Common message
+#common_message = 'Join Our 700,000+ Customers and Get Help Managing HR, Payroll, and Benefits'
+common_message = "//*[@id="block-fullwidthctasolution"]"
+Request_Pricing_onButton = //a[normalize-space()='Request Pricing']
+Find_Your_Solution = "//div[@class='field field--name-field-link field--type-link field--label-hidden field__item']//a[normalize-space()='Find Your Solution']"
+
+# common field - Footer
+footer = "//footer[@class='section']"
+
